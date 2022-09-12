0385ef43c1164973e7d5a8a79b6c82aff38877ce
Mike Züger <mike@authentiq.ch>
2022-09-12 18:56:41 +0200

    fix: wrapper

--- src/components/layout/Wrapper/Wrapper.styled.tsx
+++ src/components/layout/Wrapper/Wrapper.styled.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
 import styled from '@emotion/styled'
+import React from 'react'
+import { from } from '../../../utils/breakpoints'
 import { WrapperProps } from './Wrapper'
 
 export const Element = styled(({ fluid: _fluid, ...rest }: WrapperProps) => (
     <div {...rest} />
 ))`
     width: ${({ fluid }) => (fluid ? '100%' : undefined)};
-    max-width: ${({ fluid }) => (fluid ? undefined : 'var(--sizes-wrapper)')};
-    padding-right: var(--spacings-default);
-    padding-left: var(--spacings-default);
+    max-width: ${({ fluid }) => (fluid ? undefined : 'var(--wrapper-width)')};
+    padding-right: var(--spacing-default);
+    padding-left: var(--spacing-default);
     margin-right: auto;
     margin-left: auto;
+
+    @media ${from.tablet} {
+        padding-right: var(--spacing-large);
+        padding-left: var(--spacing-large);
+    }
 `
