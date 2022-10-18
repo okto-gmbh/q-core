0ebe343bce330198d07d1da0d2f77d5ac0ae7723
Roger Schönbächler <roger@authentiq.ch>
2022-10-18 16:21:09 +0200

    fix(SafeArea): `undefined` rendered in padding when not defined

--- src/components/layout/SafeArea/SafeArea.styled.tsx
+++ src/components/layout/SafeArea/SafeArea.styled.tsx
@@ -5,8 +5,13 @@ export const SafeArea = styled.div<SafeAreaProps>`
     position: relative;
 
     ${({ paddings }) => `
-        ${paddings?.top && 'padding-top: env(safe-area-inset-top);'}
-        ${paddings?.right && 'padding-right: env(safe-area-inset-right);'}
-        ${paddings?.bottom && 'padding-bottom: env(safe-area-inset-bottom);'}
-        ${paddings?.left && 'padding-left: env(safe-area-inset-left);'}`}
+        ${paddings?.top ? 'padding-top: env(safe-area-inset-top);' : ''}
+        ${paddings?.right ? 'padding-right: env(safe-area-inset-right);' : ''}
+        ${
+            paddings?.bottom
+                ? 'padding-bottom: env(safe-area-inset-bottom);'
+                : ''
+        }
+        ${paddings?.left ? 'padding-left: env(safe-area-inset-left);' : ''}
+    `}
 `
