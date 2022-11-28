a528df3c16e5fc758c06229ed887765e6c680b08
Mike Züger <admin@authentiq.ch>
2022-11-28 10:25:55 +0100

    fix: SafeArea fullheight

--- src/components/layout/SafeArea/SafeArea.styled.tsx
+++ src/components/layout/SafeArea/SafeArea.styled.tsx
@@ -3,11 +3,12 @@ import { SafeAreaProps } from './SafeArea'
 
 export const SafeArea = styled.div<SafeAreaProps>`
     position: relative;
+    height: 100%;
 
     ${({ top, right, bottom, left }) => `
         ${top ? 'padding-top: env(safe-area-inset-top);' : ''}
         ${right ? 'padding-right: env(safe-area-inset-right);' : ''}
         ${bottom ? 'padding-bottom: env(safe-area-inset-bottom);' : ''}
         ${left ? 'padding-left: env(safe-area-inset-left);' : ''}
-    `}
+    `};
 `
