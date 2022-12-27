5a08cd98ff894a79ac1a9f8fb73718d0cb9f954e
Andres Maier <andres@authentiq.ch>
2022-12-27 09:19:23 +0100

    feat: add db operations

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -12,5 +12,12 @@ export const deleteField = FieldValue.delete
 export { db }
 
 export const OP_EQUALS = '=='
+export const OP_NOT_EQUALS = '!='
 export const OP_GT = '>'
+export const OP_GT_OR_EQUALS = '>='
 export const OP_LT = '<'
+export const OP_LT_OR_EQUALS = '<='
+export const OP_IN = 'in'
+export const OP_NOT_IN = 'not-in'
+export const OP_CONTAINS = 'array-contains'
+export const OP_CONTAINS_ANY = 'array-contains-any'
