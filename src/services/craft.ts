3f2188208f66a1051b2669d060b20b5a0343430f
Andres Maier <andres@authentiq.ch>
2022-12-27 13:58:23 +0100

    feat: add customHeaders to craft service

--- src/services/craft.ts
+++ src/services/craft.ts
@@ -1,14 +1,18 @@
 import { GraphQLClient } from 'graphql-request'
 
 const init = ({
-    token = process.env.CRAFT_AUTH_TOKEN,
-    endpoint = process.env.CRAFT_ENDPOINT!
+    token = process.env.CRAFT_AUTH_TOKEN ??
+        process.env.NEXT_PUBLIC_CRAFT_GRAPHQL_TOKEN,
+    endpoint = (process.env.CRAFT_ENDPOINT ??
+        process.env.NEXT_PUBLIC_CRAFT_GRAPHQL_ENDPOINT)!,
+    customHeaders = {}
 }) =>
     new GraphQLClient(
         endpoint,
         token
             ? {
                   headers: {
+                      ...customHeaders,
                       Authorization: `Bearer ${token}`
                   }
               }
