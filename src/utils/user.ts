16dbc27af64f1f928c5a70cb35f17e42ecfdeb54
Andres Maier <andres@authentiq.ch>
2022-12-28 12:35:16 +0100

    feat: add privilege functions

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,13 +1,16 @@
-type BaseRoles = 'ADMIN' | 'USER'
+import { BlitzCtx } from '@blitzjs/auth'
 
-export const ROLE_ADMIN: BaseRoles = 'ADMIN'
-export const ROLE_USER: BaseRoles = 'USER'
+type BaseRole = 'ADMIN' | 'MANAGER' | 'USER'
+
+export const ROLE_ADMIN: BaseRole = 'ADMIN'
+export const ROLE_MANAGER: BaseRole = 'MANAGER'
+export const ROLE_USER: BaseRole = 'USER'
 
 export interface BaseUser {
     email: string
     hashedPassword: string
     id: string
-    role: string
+    role: BaseRole | string
 }
 
 export const mapSafeUserFields = <User extends BaseUser>(users: User[]) =>
@@ -19,3 +22,20 @@ export const getSafeUserFields: <User extends BaseUser>(
     const { hashedPassword: _hashedPassword, ...safeUserFields } = user || {}
     return safeUserFields || null
 }
+
+const hasHigherPrivileges = <User extends BaseUser>(
+    user: User,
+    { session }: BlitzCtx
+) =>
+    session.role === ROLE_ADMIN ||
+    (session.role === ROLE_MANAGER && user.role === ROLE_USER)
+
+export const filterUsersByPrivilege = <User extends BaseUser>(
+    users: User[],
+    ctx: BlitzCtx
+) => users.filter((user) => hasHigherPrivileges(user, ctx))
+
+export const hasRole = <User extends BaseUser>(
+    user: User,
+    role: BaseRole | string
+) => [ROLE_ADMIN, role].includes(user.role)
