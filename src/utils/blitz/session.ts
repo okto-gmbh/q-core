0020ed5ade263ee3d6ee2dd69a10e87b578fc0b3
Andres Maier <am@okto.ch>
2023-05-26 17:10:51 +0200

    chore: remove public data from session creation

--- src/utils/blitz/session.ts
+++ src/utils/blitz/session.ts
@@ -5,14 +5,12 @@ import { BaseUser } from '../user'
 export const createSession = async <User extends BaseUser>(
     { session }: BlitzCtx,
     { role, id, tenantId }: User
-) => {
+) =>
     await session.$create({
         role,
         tenantId,
         userId: id
     })
-    await session.$setPublicData({ userId: id })
-}
 
 export const destroySession = async ({ session }: BlitzCtx) =>
     await session.$revoke()
