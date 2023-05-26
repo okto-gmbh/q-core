82c90d52c56363deb52d1594a7334cc1e046d1ce
Andres Maier <am@okto.ch>
2023-05-26 16:56:36 +0200

    fix: set userId in $publicData

--- src/utils/blitz/session.ts
+++ src/utils/blitz/session.ts
@@ -5,12 +5,14 @@ import { BaseUser } from '../user'
 export const createSession = async <User extends BaseUser>(
     { session }: BlitzCtx,
     { role, id, tenantId }: User
-) =>
+) => {
     await session.$create({
         role,
         tenantId,
         userId: id
     })
+    await session.$setPublicData({ userId: id })
+}
 
 export const destroySession = async ({ session }: BlitzCtx) =>
     await session.$revoke()
