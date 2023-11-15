fc9acc0950dbaee00d786588eb982029411da637
Andres Maier <am@okto.ch>
2023-11-15 17:50:32 +0100

    fix: phone formatting of short numbers

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -13,6 +13,9 @@ export const formatNumber = (
 export const formatPhone = (phone?: string, locale: CountryCode = 'CH') => {
     if (!phone) return ''
 
+    // Do not format short numbers like 118
+    if (phone.length < 10) return phone
+
     const phoneParser = parsePhoneNumber(phone, locale)
 
     if (!phoneParser) return phone
