020b9c2723861d6e5f36a7ed6a69de7bcc004edf
Roger Schönbächler <rs@okto.ch>
2023-11-15 09:46:19 +0100

    feat(GridArea): independent x and y gap

--- src/components/layout/Grid/GridArea.tsx
+++ src/components/layout/Grid/GridArea.tsx
@@ -8,21 +8,29 @@ export interface GridAreaProps extends Partial<HTMLAttributes<HTMLDivElement>> {
     items: any[]
     as?: string | ComponentType<any>
     gap?: Spacing
+    gapX?: Spacing
+    gapY?: Spacing
 }
 
 const GridArea: FC<GridAreaProps> = ({
     areas,
     children,
     gap = 'default',
+    gapX = gap,
+    gapY = gap,
     ...props
-}) => (
-    <Styled.Grid
-        {...props}
-        areas={areas}
-        gap={gap}
-        items={[...new Array((children as any[]).length)]}>
-        {children}
-    </Styled.Grid>
-)
+}) => {
+    console.log('GridArea', { areas, gap, gapX, gapY })
+    return (
+        <Styled.Grid
+            {...props}
+            areas={areas}
+            gapX={gapX}
+            gapY={gapY}
+            items={[...new Array((children as any[]).length)]}>
+            {children}
+        </Styled.Grid>
+    )
+}
 
 export default GridArea
