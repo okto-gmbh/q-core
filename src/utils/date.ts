1dab31c966bf8d6b37f46bba2a4feabaca70f8dd
Andres Maier <am@okto.ch>
2024-04-10 16:11:57 +0200

    feat(date): add getRelativeTime

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -89,3 +89,26 @@ export const getExportDate = (timeZone: string = 'Europe/Zurich'): string =>
     new Date().toLocaleDateString('fr-CA', {
         timeZone
     })
+
+const cutoffs = [60, 3600, 86400, 86400 * 7, 86400 * 30, 86400 * 365, Infinity]
+const units: Intl.RelativeTimeFormatUnit[] = [
+    'second',
+    'minute',
+    'hour',
+    'day',
+    'week',
+    'month',
+    'year'
+]
+
+export function getRelativeTime(date: Date | number, lang = 'de'): string {
+    const timeMs = typeof date === 'number' ? date : date.getTime()
+    const deltaSeconds = Math.round((timeMs - Date.now()) / 1000)
+    const unitIndex = cutoffs.findIndex(
+        (cutoff) => cutoff > Math.abs(deltaSeconds)
+    )
+    const divisor = unitIndex ? cutoffs[unitIndex - 1] : 1
+
+    const rtf = new Intl.RelativeTimeFormat(lang, { numeric: 'auto' })
+    return rtf.format(Math.floor(deltaSeconds / divisor), units[unitIndex])
+}
