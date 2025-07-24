31f34424dd05b1acf2c81fbf07f0bf7f557a0582
Roger Schönbächler <rs@okto.ch>
2025-07-24 10:10:51 +0200

    fix: use `||` for fallback to work with null

--- src/utils/money.ts
+++ src/utils/money.ts
@@ -1,11 +1,11 @@
 export const formatMoney = (
-    amount: number = 0,
+    amount?: number,
     {
         locale = 'de-CH',
         ...options
     }: Intl.NumberFormatOptions & { locale?: Intl.LocalesArgument } = {}
 ) => {
-    return amount.toLocaleString(locale, {
+    return (amount || 0).toLocaleString(locale, {
         currency: 'CHF',
         maximumFractionDigits: 2,
         minimumFractionDigits: 2,
