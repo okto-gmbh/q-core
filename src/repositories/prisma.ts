699fcf766ebeab8fd95f553b2e1adf2300286de4
Andres Maier <am@okto.ch>
2026-07-22 09:38:53 +0200

    chore: remove documents

--- src/repositories/prisma.ts
+++ src/repositories/prisma.ts
@@ -24,7 +24,6 @@ export const singularTableNames: { [tableName: string]: Uncapitalize<Prisma.Mode
     contracts: 'contract',
     customers: 'customer',
     devices: 'device',
-    documents: 'document',
     domains: 'domain',
     employees: 'employee',
     firewallLists: 'firewallList',
