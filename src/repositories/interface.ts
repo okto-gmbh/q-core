9cf3f64e6312f396e36d969a91bf57d6e0aea108
Andres Maier <am@okto.ch>
2026-07-24 11:27:32 +0200

    fix: re-add software interfaces

--- src/repositories/interface.ts
+++ src/repositories/interface.ts
@@ -2,8 +2,7 @@ import type { Prisma, PrismaClient } from '@prisma/client'
 import type { GetModelByTableName, TableName } from '~core/types/models'
 import type { SwapMap } from '~core/types/type-utils'
 
-import type { Operation } from 'okto-core/db/types/prisma'
-import type { ModelName } from 'okto-core/db/types/prisma'
+import type { Operation, ModelName } from 'okto-core/db/types/prisma'
 
 import type * as operators from './operators'
 
@@ -83,7 +82,10 @@ export const tableNameModelMap = {
     rooms: 'Room',
     sessions: 'Session',
     software: 'Software',
+    softwareInterfaces: 'SoftwareInterface',
     softwareLists: 'SoftwareList',
+    softwareLogins: 'SoftwareLogin',
+    softwareServiceProviders: 'SoftwareServiceProvider',
     tasks: 'Task',
     tenants: 'Tenant',
     tokens: 'Token',
