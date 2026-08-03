837edbb179bd83e1807c739a9794d2ea4a4a852f
Andres Maier <am@okto.ch>
2026-08-03 14:29:03 +0200

    chore: submodule change

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,6 +1,7 @@
 import type { ImageLoaderProps } from 'next/image'
 
-export const customImageLoader = ({ quality, src, width }: ImageLoaderProps) =>
-    `/api/image?src=${encodeURIComponent(src)}&w=${encodeURIComponent(
+export const customImageLoader = ({ quality, src, width }: ImageLoaderProps) => {
+    return `/api/image?src=${encodeURIComponent(src)}&w=${encodeURIComponent(
         width
     )}&q=${encodeURIComponent(quality || 75)}`
+}
