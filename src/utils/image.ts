6f35a1d77ea6986f461f14a44f2de410cdd8f8d2
Andres Maier <am@okto.ch>
2026-08-19 17:33:11 +0200

    chore: revert local placeholder

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,6 +1,8 @@
 import type { ImageLoaderProps } from 'next/image'
 
 export const customImageLoader = ({ quality, src, width }: ImageLoaderProps) =>
-    `/api/image?src=${encodeURIComponent(src)}&w=${encodeURIComponent(
-        width
-    )}&q=${encodeURIComponent(quality || 75)}`
+    src === '/placeholder.png'
+        ? src
+        : `/api/image?src=${encodeURIComponent(src)}&w=${encodeURIComponent(
+              width
+          )}&q=${encodeURIComponent(quality || 75)}`
